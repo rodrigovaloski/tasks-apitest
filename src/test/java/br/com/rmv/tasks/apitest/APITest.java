b5d0a690771cac6c29c2a7ca48cc21dab54262e5
root <root@dev-stack.us-west4-b.c.hazel-logic-325718.internal>
2021-09-15 00:30:40 +0000

    First Commit

---
+++ src/test/java/br/com/rmv/tasks/apitest/APITest.java
@@ -0,0 +1,58 @@
+package br.com.rmv.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI= "http://localhost:8080/tasks-backend/";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("/todo")			
+		.then()
+			.statusCode(200)
+		;
+	}
+	
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		RestAssured.given()
+			.body("{\n"
+					+ "	\"task\" : \"Teste via API\",\n"
+					+ "	\"dueDate\" : \"2030-01-01\" \n"
+					+ "} ")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")			
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		RestAssured.given()
+			.body("{\n"
+					+ "	\"task\" : \"Teste via API\",\n"
+					+ "	\"dueDate\" : \"2000-01-01\" \n"
+					+ "} ")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")			
+		.then()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+
+}
